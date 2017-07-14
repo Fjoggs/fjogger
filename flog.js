32c484a2b98b862d5bbe57d0ea76f5141a36fd62
vegfjogs <vegard.fjogstad@finn.no>
2017-07-14 11:18:14 +0200

    Add tests

---
+++ flog.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import os from 'os';
+
+export default function log(level, msg) {
+  const file = openFile();
+  const formattedDate = formatDate(new Date());
+}
+
+export function formatDate(date) {
+  const month = date.getMonth() + 1;
+  const seconds = date.getSeconds();
+  const formatMonth = month < 10 ? `0${month}` : month;
+  const formatSeconds = seconds < 10 ? `0${seconds}` : seconds;
+  const formatDate = `[${date.getFullYear()}-${formatMonth}-${date.getDate()}] `;
+  const formatTime = `${date.getHours()}:${date.getMinutes()}:${formatSeconds}`;
+  return formatDate.concat(formatTime);
+}
+
+export function formatEntry(level, msg, date) {
+  return `${date} [${level.toUpperCase()}] ${msg}${os.EOL}`;
+}
+
+function openFile() {
+  return fs.openSync('logging/logs/log.txt', 'a');
+}
+
+function writeToFile(file, message) {
+  fs.writeSync(file, message);
+}
--- test/flogTest.js
+++ test/flogTest.js
@@ -1,15 +1,33 @@
+import { formatDate, formatEntry } from '../flog';
+import os from 'os';
+
 describe('flog', () => {
-  it('should create log file if not exists', () => {});
+  it('should format date', () => {
+    const date = new Date(1999, 11, 31, 23, 59, 59);
+    const actual = formatDate(date);
+    const expected = '[1999-12-31] 23:59:59';
+    expect(actual).toEqual(expected);
+  });
 
-  it('should append to existing file', () => {});
+  it('should add zero if month is less than 10', () => {
+    const date = new Date(1999, 7, 31, 23, 59, 59);
+    const actual = formatDate(date);
+    const expected = '[1999-08-31] 23:59:59';
+    expect(actual).toEqual(expected);
+  });
 
-  it('should format level to uppercase', () => {});
+  it('should add zero if seconds is less than 10', () => {
+    const date = new Date(1999, 11, 31, 23, 59, 1);
+    const actual = formatDate(date);
+    const expected = '[1999-12-31] 23:59:01';
+    expect(actual).toEqual(expected);
+  });
 
-  it('should format date', () => {});
-
-  it('should add zero if month is less than 10', () => {});
-
-  it('should add zero if seconds is less than 10', () => {});
-
-  it('should format log entry', () => {});
+  it('should format log entry', () => {
+    const date = new Date(1999, 11, 31, 23, 59, 59);
+    const formattedDate = formatDate(date);
+    const actual = formatEntry('info', 'test entry', formattedDate);
+    const expected = `[1999-12-31] 23:59:59 [INFO] test entry${os.EOL}`;
+    expect(actual).toEqual(expected);
+  });
 });
