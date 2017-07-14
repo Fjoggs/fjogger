c487a290bdd693c386803064984bfa9c3364c7ce
vegfjogs <vegard.fjogstad@finn.no>
2017-07-14 09:39:38 +0200

    Add jest and empty tests

---
+++ test/flogTest.js
@@ -0,0 +1,15 @@
+describe('flog', () => {
+  it('should create log file if not exists', () => {});
+
+  it('should append to existing file', () => {});
+
+  it('should format level to uppercase', () => {});
+
+  it('should format date', () => {});
+
+  it('should add zero if month is less than 10', () => {});
+
+  it('should add zero if seconds is less than 10', () => {});
+
+  it('should format log entry', () => {});
+});
